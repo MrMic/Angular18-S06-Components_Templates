24d6d514ca584f6fa9be71f670fc8eaeb39bbe66
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-28 09:43:20 +0200

    feat: Component cleanup w/ ngOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: "online" | "offline" | "unknown" = 'online';
+  private interval?: ReturnType<typeof setTimeout>;
 
   constructor() { }
 
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT');
+
+    this.interval = setInterval(() => {
       const rnd = Math.random(); //* INFO:  [0 - 1[
       if (rnd < 0.5) {
         this.currentStatus = 'online';
@@ -23,5 +26,13 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 3000)
+  }
+
+  ngAfterViewInit() {
+    console.log('afterViewInit');
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 }
