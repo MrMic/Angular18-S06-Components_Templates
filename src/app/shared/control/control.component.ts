5a34465f50b3ae164d04bbef8d66dff4daa87c99
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-06 10:18:54 +0200

    feat: ViewChild vs ContentChild

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -21,9 +21,14 @@ export class ControlComponent {
 
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+  private control = contentChild<ElementRef<
+    HTMLInputElement | HTMLTextAreaElement
+  >>('input')
 
   onClick() {
     console.log("🪚 CLICKED!:");
     console.log("🪚 el:", this.el);
+    console.log("🪚 control:", this.control());
   }
 }
