61b7aa3a4c9b1d48601a6c8803e7770a7634f16f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-10 09:37:39 +0200

    feat: Making sense of Signal Effects

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,39 +1,43 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from "@angular/core";
 
 @Component({
-  selector: 'app-server-status',
+  selector: "app-server-status",
   standalone: true,
   imports: [],
-  templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  templateUrl: "./server-status.component.html",
+  styleUrl: "./server-status.component.css",
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: "online" | "offline" | "unknown" = 'online';
+  currentStatus = signal<"online" | "offline" | "unknown">("online");
   // private interval?: ReturnType<typeof setTimeout>;
   // * INFO: Available for Angular >= 16
   private destroyref = inject(DestroyRef);
 
-  constructor() { }
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    })
+  }
 
   ngOnInit() {
-    console.log('ON INIT');
+    console.log("ON INIT");
 
     const interval = setInterval(() => {
       const rnd = Math.random(); //* INFO:  [0 - 1[
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set("online");
       } else if (rnd < 0.8) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set("offline");
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set("unknown");
       }
-    }, 3000)
+    }, 3000);
 
     this.destroyref.onDestroy(() => clearInterval(interval));
   }
 
   ngAfterViewInit() {
-    console.log('afterViewInit');
+    console.log("afterViewInit");
   }
 
   // ngOnDestroy() {
