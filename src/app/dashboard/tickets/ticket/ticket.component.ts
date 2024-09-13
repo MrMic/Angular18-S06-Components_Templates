90c255d8db007c4060d7c42bd743b84057c64db7
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-13 09:41:51 +0200

    feat(UI/UX): Revisit Inputs & Signals

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
 }
